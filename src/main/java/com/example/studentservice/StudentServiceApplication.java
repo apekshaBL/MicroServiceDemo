c001d2e2e6b9277e50ed4fb79de1a4939adc4035
Apeksha Lokhande <apekshalokhande8600@gmail.com>
2026-02-10 16:16:42 +0530

    changes done

--- src/main/java/com/example/studentservice/StudentServiceApplication.java
+++ src/main/java/com/example/studentservice/StudentServiceApplication.java
@@ -2,15 +2,18 @@ package com.example.studentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient; // <--- THIS WAS MISSING
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration.class})
 @EnableDiscoveryClient
-@ComponentScan(basePackages = {"com.example.studentservice", "com.example.common"})
+@ComponentScan(basePackages = {"com.example"}) // Broaden to pick up common-lib
+@EntityScan(basePackages = {"com.example"})    // Find entities in common-lib
+@EnableJpaRepositories(basePackages = {"com.example"}) // Find repos in common-lib
 public class StudentServiceApplication {
     public static void main(String[] args) {
-
         SpringApplication.run(StudentServiceApplication.class, args);
     }
 }
