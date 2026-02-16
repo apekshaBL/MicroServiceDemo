6f252ada82e672d9034a9fb9bb3e6b78b6159976
Apeksha Lokhande <apekshalokhande8600@gmail.com>
2026-02-16 11:19:18 +0530

    Removed course, payment and student services

--- payment-service/src/main/java/payment_service/repository/PaymentRepository.java
+++
@@ -1,6 +0,0 @@
-package payment_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import payment_service.entity.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> { }
