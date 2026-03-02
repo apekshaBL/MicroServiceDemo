848b6a788ed3a5b4dd7918c385a5eda548e682bb
Apeksha Lokhande <apekshalokhande8600@gmail.com>
2026-03-02 10:01:46 +0530

    done

--- notification-service/src/main/java/notification_service/NotificationServiceApplication.java
+++ notification-service/src/main/java/notification_service/NotificationServiceApplication.java
@@ -2,12 +2,14 @@ package notification_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"notification_service", "LoggingAspect"})
+@SpringBootApplication
+@EnableJpaRepositories("notification_service.repository")
+@EntityScan("notification_service.entity") // Changed from .model to .entity
 public class NotificationServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
-
 }
